c92fad72e16ea5637dc90e8ef991b6b81ab7c1b3
Aymetic <m343894011@gmail.com>
2021-05-04 15:40:02 +0800

    添加文字

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <h1 class="text-9xl text-center text-blue-600">hello world</h1>
-  <div align="center"><img alt="Vue logo" src="./assets/logo.png" /></div>
+  <div align="center"><img alt="Vue logo" src="./assets/mylogo.png" /></div>
   <HelloWorld msg="Hello Vue 3.0 + Vite" />
 </template>
 
