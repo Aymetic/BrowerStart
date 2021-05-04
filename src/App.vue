dbb7dfb271a64cb0bdebc0c148612d72af15b764
Aymetic <m343894011@gmail.com>
2021-05-04 18:04:55 +0800

    commit

--- src/App.vue
+++ src/App.vue
@@ -2,15 +2,20 @@
   <h1 class="text-9xl text-center text-blue-600">hello world</h1>
   <div align="center"><img alt="Vue logo" src="./assets/mylogo.png" /></div>
   <HelloWorld msg="Hello Vue 3.0 + Vite" />
+  <div>
+    <FlowButton msg="搜索" />
+  </div>
 </template>
 
 <script>
 import HelloWorld from "./components/HelloWorld.vue";
+import FlowButton from "./components/FlowButton.vue";
 
 export default {
   name: "App",
   components: {
     HelloWorld,
+    FlowButton,
   },
 };
 </script>
---
+++ src/components/FlowButton.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="text-center">
+    <input type="" />
+    <button
+      @click="count++"
+      class="mx-2 h-9 w-32 border border-solid border-purple-500 rounded-full"
+    >
+      {{ msg }}
+    </button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "FlowButton",
+  props: {
+    msg: String,
+  },
+  data() {
+    return {
+      count: 0,
+    };
+  },
+};
+</script>
